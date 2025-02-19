75575b310f370b530ef6cedbae1bbefee2fbc49b
Hasrh-Mahajan8 <hasrhmahajansk@gmail.com>
2025-02-19 19:46:06 +0530

    adding error handling middlewares

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const mongoose = require('mongoose');
 const path = require('path');
 const Listing = require('./models/listing');
 const ejsMate = require('ejs-mate');
-
+//errorHangling Imports
+const wrapAsync = require('./util/wrapAsync.js');
+const expressError = require("./util/expressError.js");
+const joi = require('joi'); 
+const {listingSchema} = require('./schema.js');
 //setting ejs
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,"views"));
@@ -25,6 +29,14 @@ main().then((res) => {console.log('server is working')})
 app.listen(8080, (req,res) => {
     console.log("server is working");
 })
+//validateListing
+const validateListing = (req, res, next) => {
+    let {error} = listingSchema.validate(req.body);
+    if(error){
+        throw new expressError(400, error)
+    }
+    next()
+}
 
 //home route
 app.get('/', (req, res) => {
@@ -32,11 +44,12 @@ app.get('/', (req, res) => {
 })
 
 //index route
-app.get('/listings',async (req,res) => {
+app.get('/listings',validateListing,
+    wrapAsync(async (req,res) => {
     let allListing =await Listing.find({});
     console.log('index route working');
     res.render('listings/index.ejs',{allListing});
-})
+}));
 
 
 //add button
@@ -45,54 +58,62 @@ app.get('/listings/new', (req,res) => {
     res.render('listings/new.ejs');
 })
 //post route
-app.post('/listings',async (req,res) => {
-    let newListing = new Listing(req.body);
-    await newListing.save().then((res) => {console.log(res)}).catch((err) => {console.log('err in saving data',err)});   
-    console.log('post route working');
-    res.redirect('/listings');
-})
+app.post('/listings',validateListing,wrapAsync(async (req,res, next) => {
+        let newListing = new Listing(req.body);
+        await newListing.save();
+        console.log('post route working');
+        res.redirect('/listings');
+
+}))
 //update btn
-app.get('/listings/:id/edit',async (req,res) => {
+app.get('/listings/:id/edit',validateListing,wrapAsync(async (req,res) => {
+    if(!req.body.listing){
+        throw new expressError(400,"Send a valid listing");
+    };
     let { id } = req.params;
     console.log(id);
     let data = await Listing.findById(id);
     console.log('update btn working');
     res.render('listings/edit.ejs',{data});
-})
+}));
 //save Update
-app.put('/listings/:id',async (req,res) => {
+app.put('/listings/:id',validateListing,wrapAsync(async (req,res) => {
     let { id } = req.params;
     console.log(id);
     let updatedData = req.body;
     console.log("Update saved");
     let data = await Listing.findByIdAndUpdate(id,updatedData,{new:true});
     res.redirect(`/listings/${id}`);
-})
+}));
 
 //delete route
-app.delete('/listings/:id',async (req,res) => {
+app.delete('/listings/:id',wrapAsync(async (req,res) => {
     let { id } = req.params;
     await Listing.findByIdAndDelete(id).then((res)=>{console.log('Listing Deleted')}).catch((err) => {console.log('err in deleting route')});
     res.redirect('/listings');
-})
+}));
 
 //show route
-app.get("/listings/:id",async (req,res) => {
+app.get("/listings/:id",validateListing,wrapAsync(async (req,res) => {
     let { id } = req.params;
     if (!mongoose.Types.ObjectId.isValid(id)) {
         console.log('Invalid ObjectId');
         return res.status(400).send('Invalid ID');
       }
-      try {
         let listingData = await Listing.findById(id);
-        if (!listingData) {
-          return res.status(404).send('Listing not found');
-        }
         console.log('show route working');
         res.render('listings/show.ejs', { listingData });
-      } catch (err) {
-        console.log('Error in show route', err);
-        res.status(500).send('Server Error');
       }
-    });
+    ));
+
+    app.all('*', (req, res, next) => {
+        throw new expressError(404,"Page not found!!");
+    })
+
+    //error middleware
+    app.use((err,req, res, next) => {
+        let { status=500, message = "Somethingn went wrong" } = err;
+        // res.status(status).send(message);
+        res.render('error.ejs', { message });
+    })
 
