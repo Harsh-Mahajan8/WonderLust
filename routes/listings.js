14ba564bf1d2e19e3706ad3aa482a339f0db6307
Hasrh-Mahajan8 <hasrhmahajansk@gmail.com>
2025-03-01 12:28:01 +0530

    storing file on cloud using cloudinary and parser-multer

--- routes/listings.js
+++ routes/listings.js
@@ -3,11 +3,16 @@ const router = express.Router();
 const wrapAsync = require('../util/wrapAsync.js');
 const { isLoggedIn, isOwner, validateListing } = require('../middleware.js');
 const listingController = require('../controller/listing.js');
-
+const multer = require('multer');
+const {storage} = require('../cloudConfig');
+const upload  = multer({storage});
 router
     .route('/')
     .get(wrapAsync(listingController.index))//index route
-    .post(validateListing, wrapAsync(listingController.saveListing));//post route
+    // .post(validateListing, wrapAsync(listingController.saveListing));//post route
+    .post(upload.single("image"), (req, res) => {
+        res.send(req.file);
+    });  
 
 //add button
 router.get('/new', isLoggedIn, listingController.newListing);
